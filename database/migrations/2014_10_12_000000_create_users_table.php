0bcf49641c74f181680fa3244a832a9a38afe6b5
MarcoChavezB <marco1102004@gmail.com>
2024-05-09 16:59:26 -0600

    añadiendo migraciones

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,11 +16,27 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('last_name');
+            $table->string('user_identification')->unique();
+            $table->string('photo');
+            $table->string('phone');
+            $table->string('cellphone');
+            $table->string('curp');
+            $table->string('email');
+            $table->timestamp('emergency_contact');
+            $table->string('emergency_phone');
+            $table->string('emergency_direction');
+            $table->string('user_type');
             $table->string('password');
+            $table->string('credit');
             $table->rememberToken();
             $table->timestamps();
+
+            $table->unsignedBigInteger('id_created_by');
+            $table->foreign('id_created_by')->references('id')->on('users');
+
+            $table->unsignedBigInteger('id_base');
+            $table->foreign('id_base')->references('id')->on('bases');
         });
     }
 
