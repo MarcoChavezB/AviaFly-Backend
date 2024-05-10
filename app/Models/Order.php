0416bc9dc237e1722e6ced833da344047b0f3dbb
MarcoChavezB <marco1102004@gmail.com>
2024-05-09 18:46:44 -0600

    añadiendo relaciones en modelos order y product

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'order_date',
+        'total',
+        'id_employe',
+        'id_customer'
+    ];
+
+    public function employe()
+    {
+        return $this->belongsTo(User::class, 'id_employ');
+    }
+
+    public function customer()
+    {
+        return $this->belongsTo(User::class, 'id_customer');
+    }
 }
