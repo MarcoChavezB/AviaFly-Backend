41cf8d25e671e13490b762cacf7c674bc9b7bc5f
Alonso <abrahamreynoso64@gmail.com>
2024-05-28 19:15:47 -0600

    Metodos y endpoints para modificar parametros de materias

--- routes/api.php
+++ routes/api.php
@@ -97,6 +97,7 @@ Route::prefix('/instructor')->group(function () {
         Route::get('/get/students', [InstructorController::class, 'getStudentsByInstructor']); // Esto puede hacerlo: instructor
         Route::get('/get/instructors-subjects', [InstructorController::class, 'getInstructorsSubjects']); // Esto puede hacerlo: root, admin
         Route::put('/update/instructors-subjects', [InstructorController::class, 'updateInstructorsSubjects']); // Esto puede hacerlo: root, admin
+        Route::put('/update/student/grade', [InstructorController::class, 'updateStudentGrade']); // Esto puede hacerlo: instructor
     });
 });
 
