5cb18adc3b1ea7d05df3367ec971745571cab56c
Alonso <abrahamreynoso64@gmail.com>
2024-07-12 13:44:11 -0600

    Bloque de codigo para crear el directorio para el estudiante

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -113,6 +113,14 @@ class StudentController extends Controller
             $student->user_identification = 'A' . $base->name[0] . $student->id;
             $student->save();
 
+            $baseName = strtolower($base->name);
+            $baseName = str_replace(['á', 'é', 'í', 'ó', 'ú'], ['a', 'e', 'i', 'o', 'u'], $baseName);
+            $folderPath = public_path("bases/{$baseName}/{$student->user_identification}");
+
+            if (!file_exists($folderPath)) {
+                mkdir($folderPath, 0777, true);
+            }
+
             $user = new User();
             $user->user_identification = $student->user_identification;
             $user->password = bcrypt($student->curp);
