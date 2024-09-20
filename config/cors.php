b4d8a55c2db2f65f51ac256e8ef05fdc75de1ac9
MarcoChavezB <marco1102004@gmail.com>
2024-09-19 20:33:07 -0600

    modificando cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://3.138.175.65:80'],
+    'allowed_origins' => ['http://3.138.175.65'],
 
     'allowed_origins_patterns' => [],
 
