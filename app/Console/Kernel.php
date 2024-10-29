6bd01235d03990b40c74ab39e9fa4c5e1ff2182e
MarcoChavezB <marco1102004@gmail.com>
2024-10-29 10:28:12 -0600

    quitando job

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,7 +17,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->job(new EmployeeRecordJob)->everyMinute();
+/*         $schedule->job(new EmployeeRecordJob)->everyMinute(); */
     }
 
     /**
