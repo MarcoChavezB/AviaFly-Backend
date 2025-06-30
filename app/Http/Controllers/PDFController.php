3f7fa566e46d1ee88cfcfcd8fc7d818ad726db14
MarcoChavezB <marco1102004@gmail.com>
2025-06-30 14:39:21 -0600

    quitando variable price

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -236,7 +236,7 @@ class PDFController extends Controller
             $response[$result->id_order]['items'][] = [
                 'quantity' => number_format($result->quantity, 2, '.', ''),
                 'concept' => $result->item,
-                'price' => $result->pice,
+/*                 'price' => $result->pice, */
                 'payment_method' => $result->payment_method,
                 'total' => number_format($result->price, 2, '.', '') * $result->quantity,
             ];
