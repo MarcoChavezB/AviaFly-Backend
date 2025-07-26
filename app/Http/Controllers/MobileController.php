87d8626e6c337e0cf18aee455c2a59baffd0e0c1
MarcoChavezB <marco1102004@gmail.com>
2025-07-26 11:29:35 -0600

    agregando nuevo control

---
+++ app/Http/Controllers/MobileController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\flightHistory;
+use App\Models\NewSletter;
+use App\Models\Student;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
+class MobileController extends Controller
+{
+}
