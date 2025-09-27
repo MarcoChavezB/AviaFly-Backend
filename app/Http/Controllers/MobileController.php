4b2dbe5e390e1b1df5b84484825b46172a95f054
MarcoChavezB <marco1102004@gmail.com>
2025-09-26 20:24:13 -0600

    agregando cambios

--- app/Http/Controllers/MobileController.php
+++ app/Http/Controllers/MobileController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\flightHistory;
 use App\Models\NewSletter;
 use App\Models\Student;
+use App\Models\StudentSubject;
+use App\Models\TeacherSubjectTurn;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -14,5 +16,23 @@ class MobileController extends Controller
 {
     public function getTeacherData(){
         $teacher = Auth::user();
+
+        $subjects = TeacherSubjectTurn::with('subject')
+            ->where('id_teacher', $teacher->id)
+            ->get();
+
+        $students = StudentSubject::with('student')
+            ->where('id_teacher', $teacher->id)
+            ->get();
+
+        $data = [
+            'subjects' => $subjects,
+            'students' => $students
+        ];
+
+        return response()->json([
+            'success' => true,
+            'data' => $data
+        ]);
     }
 }
